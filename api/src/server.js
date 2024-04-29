a6a67907f0c8f9b807ccc72e37957a44a144a9d3
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-04-29 12:42:51 -0300

    add listen port

--- api/src/server.js
+++ api/src/server.js
@@ -1,1 +1,11 @@
+// importando o express do modulo do nodemodules
 const express = require('express');
+
+// inicializando express para utilização 
+const app = express();
+
+// criando uma porta para escutar requisições e solicitações
+const port = 3333;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+});
