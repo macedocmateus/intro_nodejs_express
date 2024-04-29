f7ae445c2533f5c6f4f9c9aecd6bbf15af30d3a2
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-04-29 13:20:57 -0300

    add request and response with route params test

--- api/src/server.js
+++ api/src/server.js
@@ -4,6 +4,16 @@ const express = require('express');
 // inicializando express para utilização 
 const app = express();
 
+// requisição de leitura com get 
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params; // desestruturando o request.params.id e request.params.user
+
+    response.send(`
+    id da mensagem: ${id}, 
+    para o usuário: ${user}
+    `);
+});
+
 // criando uma porta para escutar requisições e solicitações
 const port = 3333;
 app.listen(port, () => {
