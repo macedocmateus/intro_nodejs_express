43b0651a6f3ba310519d67b7cbd866a29fd00be7
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-04-30 11:00:29 -0300

    add POST request method

--- api/src/server.js
+++ api/src/server.js
@@ -4,23 +4,10 @@ const express = require('express');
 // inicializando express para utilização 
 const app = express();
 
-// requisição de leitura com get
-// os routes params são obrigatórios 
-app.get("/message/:id/:user", (request, response) => {
-    const { id, user } = request.params; // desestruturando o request.params.id e request.params.user
+//  
+app.post("/users", (request, response) => {
 
-    response.send(`
-    id da mensagem: ${id}, 
-    para o usuário: ${user}
-    `);
-});
-
-// query params são opcionais quando não forem passados eles são undefined
-// caso tenha mais de um parâmetros /user ?page = valor  & limit = valor 
-app.get("/user", (request, response) => {
-    const { page, limit } = request.query;
-
-    response.send(`página: ${page}  limite é esse: ${limit}`);
+    response.send("você chamou o post");
 });
 
 // criando uma porta para escutar requisições e solicitações
