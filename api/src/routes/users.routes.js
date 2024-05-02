775da797780c8b1c645ace2531494c482035cd5c
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-05-01 22:52:25 -0300

    add create into user-controller

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
+const UsersController = require("../controllers/UsersControllers");
+
 const usersRoutes = Router();
 
-usersRoutes.post("/", (request, response) => {
-    const { name, email, password } = request.body;
+const usersController = new UsersController();
 
-    response.json({ name, email, password });
-});
+
+usersRoutes.post("/", usersController.create);
 
 // exportando o arquivo para utilização
 module.exports = usersRoutes;
