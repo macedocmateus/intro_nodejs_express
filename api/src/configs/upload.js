625e5a9f77c28c69a988839c8e46222bcd893856
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-07-08 17:20:32 -0300

    add upload files

---
+++ api/src/configs/upload.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const multer = require('multer'); // multer é biblioteca para fazer uploads
+const crypto = require('crypto'); // função hash para criar arquivo com nome único
+
+const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp'); // é onde a imagem chega
+const UPLOADS_FOLDER = path.resolve(__dirname, 'uploads'); // é onde a imagem vai ficar ou seja, destino
+
+const MULTER = {
+    storage: multer.diskStorage({
+        destination: TMP_FOLDER,
+        filename(request, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString('hex'); // usando crypto para não criar arquivo de mesmo nome, a função vai sobrescrever se caso isso aconteça
+
+            const fileName = `${fileHash}-${file.originalname}`; // evitar nomes iguais
+
+            return callback(null, fileName);
+        },
+    }),
+};
+
+module.exports = {
+    TMP_FOLDER,
+    UPLOADS_FOLDER,
+    MULTER,
+};
