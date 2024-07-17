b4af2633fcd1f42c69dd1b2fe709f6c684cd6ed9
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-07-17 07:31:46 -0300

    add show avatar img

--- api/src/server.js
+++ api/src/server.js
@@ -1,16 +1,17 @@
 // importação do novo pacote instalado express async errors
-require("express-async-errors");
+require('express-async-errors');
 
 // importando a classe AppError
-const AppError = require("./utils/AppError");
+const AppError = require('./utils/AppError');
+
+uploadConfig = require('./configs/upload');
 
 const express = require('express');
 
 const routes = require('./routes');
 
 // importando o banco de dados
-const migrationsRun = require("./database/sqlite/migrations")
-
+const migrationsRun = require('./database/sqlite/migrations');
 
 const app = express();
 app.use(express.json());
@@ -18,6 +19,8 @@ app.use(express.json());
 app.use(routes);
 
 migrationsRun();
+
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 
 // captura de erros, requisições, as respostas e o prosseguir
 // só os parâmetros error e response estão em uso, pois next não vai avançar quando tiver o erro
@@ -27,22 +30,21 @@ app.use((error, request, response, next) => {
     // erro do lado do cliente
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        })
+            status: 'error',
+            message: error.message,
+        });
     }
 
     console.error(error);
 
     // erro do lado do servidor
     return response.status(500).json({
-        status: "error",
-        message: "Internal server error"
-    })
+        status: 'error',
+        message: 'Internal server error',
+    });
 });
-
 
 const port = 3333;
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${port}`);
 });
